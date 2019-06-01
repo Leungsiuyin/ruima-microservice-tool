01eefb4a8fc6821fa9945abf5f306cfd514c64a6
LeungSiuYin <670672042@qq.com>
2019-06-01 09:33:33 +0800

    first init push

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MicroserviceTool;
+
+class Index {
+  public function test ()
+  {
+    # code...
+    echo 'hello word';
+  }
+}
---
+++ test.php
@@ -0,0 +1,8 @@
+
+<?php
+require 'vendor/autoload.php';
+ 
+use MicroserviceTool\index;
+ 
+$miniUpload = new index();
+$miniUpload->test();
